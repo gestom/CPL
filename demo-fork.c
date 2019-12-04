01eed407caa33b3672380e785897c14d3abe2bc6
Tom Krajnik <tomkrajnik@hotmail.com>
2019-12-04 10:23:40 +0100

    New feature

--- demo-fork.c
+++ demo-fork.c
@@ -12,22 +12,6 @@ void compute(int myid, int n);
 
 int main(int argc, char *argv[])
 {
-   int n = argc > 1 ? atoi(argv[1]) : 10000;
-   for (int i = 0; i < NUMPROCS; ++i) {
-      pid_t pid = fork();
-      if (pid == 0) {
-         compute(i, n);
-         exit(0);
-      } else {
-         printf("Main -- new child: %d has been created\n", pid);
-      }
-   }
-   printf("All processes created\n");
-   for (int i = 0; i < NUMPROCS; ++i) {
-      int r;
-      pid_t pid = wait(&r);
-      printf("Wait for pid %d return: %d\n", pid, r);
-   }
    return 0;
 }
 
